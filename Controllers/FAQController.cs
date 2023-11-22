d6d208efe6aecc9d6abdb4b39e1652fa4e73015f
jjuarez5 <julio.juarez5@outlook.com>
2023-11-22 16:56:41 -0600

    adjust controller to use new client

--- Controllers/FAQController.cs
+++ Controllers/FAQController.cs
@@ -2,26 +2,28 @@
 {
     using Microsoft.AspNetCore.Mvc;
 
-
     [Route("api/[controller]")]
     [ApiController]
     public class FAQController : ControllerBase
     {
-        private HttpClient _httpClient;
+        //private HttpClient _httpClient;
         private IConfiguration _configuration;
         private readonly FAQClient _client;
+        private readonly ChatGPTClient _chatGPTClient;
 
-        public FAQController( HttpClient client, IConfiguration configuration)
+        public FAQController(IConfiguration configuration,  ChatGPTClient chatGPTClient)
         {
-            this._httpClient = client;
+            //this._httpClient = client;
             this._configuration = configuration;
-            this._client = new FAQClient(this._httpClient, this._configuration);
+            //this._client = new FAQClient(this._httpClient, this._configuration);
+            //this._openAiClient = openAiClient;
+            this._chatGPTClient = new ChatGPTClient(this._configuration);
         }
 
         [HttpPost]
         public async Task<object> GetFAQAnswer( [FromBody] Message message)
         {
-            var response = await this._client.GetChatResponseAsync(message).ConfigureAwait(false);
+            var response = await this._chatGPTClient.GetChatCompletionAsync(message);
 
             return response;
 
