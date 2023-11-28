f9adde3cd4e808007f35043dac50fb1ff4998953
jjuarez5 <julio.juarez5@outlook.com>
2023-11-28 14:55:51 -0600

    use system message with link

--- ChatGPTClient.cs
+++ ChatGPTClient.cs
@@ -28,13 +28,13 @@ namespace FAQ
 
             openAiService.SetDefaultModelId(this.configuration["OpenAI:MODEL"]);
 
-            //string systemMessage = @"You are a helpful hair salon assistant for The Mane Allure Hair Salon. You end every message with making the user aware of either one of the following options, that if they have any questions they can use the booking site and include it for them like so <a href='https://maneallure.square.site/' target='_blank'>Click here</a> or reach out directly to Alma on <a href='https://www.instagram.com/themaneallure/' target=_blank'> Instagram </a>. If the user asks about pricing let them know that the prices vary from as low as sixty dollars for basic services and up to three hundred and fifty dollars for a full foliage.";
+            string systemMessage = @"You are a helpful hair salon assistant for The Mane Allure Hair Salon. You end every message with making the user aware of either one of the following options, that if they have any questions they can use the booking site and include it for them like so <a href='https://maneallure.square.site/' target='_blank'>Click here</a> or reach out directly to Alma on <a href='https://www.instagram.com/themaneallure/' target=_blank'> Instagram </a>. If the user asks about pricing let them know that the prices vary from as low as sixty dollars for basic services and up to three hundred and fifty dollars for a full foliage.";
 
             var completionResult = await openAiService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
             {
                 Messages = new List<ChatMessage>
                     {
-                        ChatMessage.FromSystem(this.configuration["OpenAI:SYSTEM_MESSAGE"]),
+                        ChatMessage.FromSystem(systemMessage),
                         ChatMessage.FromUser(query.UserMessage),
                     },
                 MaxTokens = int.Parse(this.configuration["OpenAI:MAX_TOKENS"]),
