d446acf128cfb797446c183ff60bca9b3fbefc80
jjuarez5 <julio.juarez5@outlook.com>
2023-11-28 14:46:45 -0600

    cleanup

--- Program.cs
+++ Program.cs
@@ -6,27 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 IConfiguration configuration = builder.Configuration;
 
-/// <summary>
-/// Creates an HTTP Client. 
-/// </summary>
-/// <returns></returns>
-//async Task<HttpClient> InitializeFAQHttpClient( IConfigurationSection configurationSection )
-//{
-//    string apiKey = configurationSection.GetSection("API_KEY").Value;
-//    string openAiUrl = configurationSection.GetSection("API_URL").Value;
-//    HttpClient client = new HttpClient();
-//    client.BaseAddress = new Uri(openAiUrl);
-//    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
-//    return client;
-//}
-
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins(configuration["AllowedHosts"])
+                          policy.WithOrigins("https://www.themaneallure.com/#chat", "https://www.themaneallure.com")
                                             .AllowAnyHeader()
                                             .AllowAnyMethod();
                       });
@@ -36,12 +22,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddOpenAIService<ChatGPTClient>(settings =>
-//{
-//    settings.ApiKey = configuration["OpenAI:API_KEY"];
-//    settings.Model = configuration["OpenAI:MODEL"]
-
-//});
 //builder.Services.AddSingleton<HttpClient>(InitializeFAQHttpClient(configuration.GetSection("OpenAi")).GetAwaiter().GetResult());
 //builder.Services.AddSingleton<IFAQClient, FAQClient>();
 builder.Services.AddSingleton<ChatGPTClient>();
